f437f49436e4997911f735b31f262cdb76103ed3
Gustavo Martins <gustavosm994@gmail.com>
2026-04-22 17:17:28 -0300

    health check

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,8 @@ import fastify from 'fastify'
 
 const app = fastify()
 
+app.get('/health', async (_request, reply) => {
+  return await reply.send({ status: 'Everthing is working' })
+})
+
 export { app }
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import { app } from './app.js'
 
-app.listen({
-  port: 4444,
-  host: '0.0.0.0',
-})
+app.listen(
+  {
+    port: 4444,
+    host: '0.0.0.0',
+  },
+  () => {
+    console.log('Server is running on port 4444')
+  },
+)
