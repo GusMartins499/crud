8c0ac07c520ec34e20935a5cb745fb76bfc72868
Gustavo Martins <gustavosm994@gmail.com>
2026-04-23 20:10:02 -0300

    feat/format-brl-price

--- src/modules/products/products.dto.ts
+++ src/modules/products/products.dto.ts
@@ -1,4 +1,4 @@
-import { priceFromCents } from '../../utils/price-in-cents.js'
+import { formatPrice, priceFromCents } from '../../utils/price-in-cents.js'
 import type { TDrizzleProduct, TProductDTO } from './products-types.js'
 
 export function productToDto(product: TDrizzleProduct): TProductDTO {
@@ -6,5 +6,6 @@ export function productToDto(product: TDrizzleProduct): TProductDTO {
     id: product.id,
     name: product.name,
     price: priceFromCents(product.price_cents),
+    formatPrice: formatPrice(product.price_cents),
   }
 }
--- src/utils/price-in-cents.ts
+++ src/utils/price-in-cents.ts
@@ -1,3 +1,12 @@
 export const priceInCents = (price: number) => Math.round(price * 100)
 
 export const priceFromCents = (priceCents: number) => priceCents / 100
+
+export const formatPrice = (priceInCents: number) => {
+  const price = priceFromCents(priceInCents)
+
+  return Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(price)
+}
