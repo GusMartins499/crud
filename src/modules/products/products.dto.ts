50a5deb0e705ad2844daa9af5868631402b16188
Gustavo Martins <gustavosm994@gmail.com>
2026-04-23 19:30:37 -0300

    fix dto mapping price

--- src/modules/products/products.dto.ts
+++ src/modules/products/products.dto.ts
@@ -1,10 +1,10 @@
-import { priceInCents } from '../../utils/price-in-cents.js'
+import { priceFromCents } from '../../utils/price-in-cents.js'
 import type { TDrizzleProduct, TProductDTO } from './products-types.js'
 
 export function productToDto(product: TDrizzleProduct): TProductDTO {
   return {
     id: product.id,
     name: product.name,
-    price: priceInCents(product.price_cents),
+    price: priceFromCents(product.price_cents),
   }
 }
