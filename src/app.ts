1e5cc2c3eef9d95d244b8971dd05640a1f42e8ee
Gustavo Martins <gustavosm994@gmail.com>
2026-04-28 18:41:40 -0300

    wip

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,38 @@
+import fastifySwagger from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
 import fastify from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
 import { ZodError } from 'zod'
 import { productsRoutes } from './modules/products/routes.js'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Vitatrack API',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+  uiConfig: {
+    syntaxHighlight: {
+      activate: true,
+      theme: 'tomorrow-night',
+    },
+  },
+})
 
 app.get('/health', async (_request, reply) => {
   return await reply.send({ status: 'Everthing is working' })
